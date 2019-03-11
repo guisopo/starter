e155b6fd8431eafdcddea7e597287e695320e54b
guisopo <solerguillermo@hotmail.com>
2019-03-11 12:13:01 +0100

    Added clean-webpack-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 const path = require("path");
 
 module.exports = merge(common, {
   mode: "production",
   output: {
     filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+      new CleanWebpackPlugin()
+  ]
 });
