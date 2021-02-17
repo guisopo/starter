f51f704bc2d590fc9560396813ad4489544b8dca
guisopo <solerguillermo@hotmail.com>
2021-02-17 11:52:09 +0100

    Delete package.json and webpack files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { consoleName } from "./utils/console";
 import "./main.scss";
+import { consoleName } from "./utils/console";
 consoleName('Guillermos');
 
 document.addEventListener('click', () => console.log('hola'));
